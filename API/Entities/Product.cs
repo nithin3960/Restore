2d68f82f209bbab0ee102e35b3b86211dbf7a919
Nrayudu <nithinmanohar.rayudu@gmail.com>
2025-02-15 15:16:57 -0800

    End of section 2

---
+++ API/Entities/Product.cs
@@ -0,0 +1,20 @@
+namespace API.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required String Name { get; set;}
+
+    public required String Description { get; set;}
+
+    public required long Price { get; set;}
+
+    public required String PictureUrl {get; set; }
+
+    public required String Type { get; set; }
+
+    public required String Brand { get; set; }
+
+    public int QuantityInStock { get; set; }
+
+}
